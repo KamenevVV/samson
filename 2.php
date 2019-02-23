0c0dba9785a2eccc40d5946301bb862c939ba146
KamenevVV <47820947+KamenevVV@users.noreply.github.com>
2019-02-23 13:14:09 +0300

    first change importXML

--- 2.php
+++ 2.php
@@ -49,6 +49,7 @@ function importXml( string $a)
 {
     if ( !is_file( $a ) ) return false;
     $xml = simplexml_load_file($a);
+    
     // установим соедиение с базой данных и кодировку
     $mysqli = new mysqli('localhost', 'mysql', 'mysql', 'test_samson');
     if (mysqli_connect_errno())
@@ -57,16 +58,17 @@ function importXml( string $a)
     exit();
     }
     $query = $mysqli->query('set names utf8');
-    // самый простой способ "запросы в цикле" нам не подходит, поэтому:
+    
     // добавим счётчик итераций, который будем использовать в качестве суррогатного первичного ключа товара
     // так как ни имя ни код товара не уникальны
-    $surr = 0;             //
-    $product_bind[]  = ''; //
-    $price_bind[]    = ''; //
-    $property_bind[] = ''; //
-    $category_bind[] = ''; //
-    $prod_cat_bind[] = ''; //
+    $surr = 0;             // ключ обеспечивающий уникальность товара и связи с параметрами товара на этапе вставки в БД 
+    $product_bind[]  = ''; // первая строка массива параметров используемая для хранения литералов типа переменных для таблицы a_product
+    $price_bind[]    = ''; // первая строка массива параметров используемая для хранения литералов типа переменных для таблицы a_price
+    $property_bind[] = ''; // первая строка массива параметров используемая для хранения литералов типа переменных для таблицы a_property
+    $category_bind[] = ''; // первая строка массива параметров используемая для хранения литералов типа переменных для таблицы a_category
+    $prod_cat_bind[] = ''; // первая строка массива параметров используемая для хранения литералов типа переменных для таблицы связи a_prod_cat
     
+    // парсим XML файл и собираем массивы параметров
     foreach ( $xml->Товар as $p )
     {
         $surr++;
@@ -75,7 +77,7 @@ function importXml( string $a)
         $product_bind[0] .= str_pad('', 2, 'is');
         $product_bind[] = $code;
         $product_bind[] = $name;
-        $product[$surr] = $name;
+        $product[$surr] = $name; // этот массив будет содержать соответсвие суррогатного ключа реальному ключу из БД
         
         foreach ( $p->Цена as $c )
         {
@@ -105,33 +107,34 @@ function importXml( string $a)
             $category[][$surr] = $cat;
         }
     }
+    
     $category_unique = array_unique($category_unique);
 
-        // таблица категорий не имеет внешних ключей, добавление в БД начнём с неё
-        // сформируем запрос SELECT что бы знать какие разделы из файла XML уже записаны в БД
-        $query = "SELECT `category_id`,`category_name` 
-                    FROM `a_category` 
-                    WHERE `category_name` IN ("
-                    . implode(',', array_fill( 1, count( $category_unique ), '?'  ) ) 
-                    ." )";
-        foreach($category_unique as $key => $value)
-            $category_unique[$key] = &$category_unique[$key];
-        array_unshift( $category_unique, str_pad('', count($category_unique), 's') );
-        $stmt = $mysqli->prepare( $query );
-        call_user_func_array([$stmt, 'bind_param'], $category_unique);
-		$stmt->execute();
-        unset( $category_unique[0] );      
-        $stmt->bind_result ( $category_id, $category_name );
-        while ( $stmt->fetch() )
-        {
-            // сохраняем результат выборки, что бы не доставать эти записи повторно
-            // этот массив будет использоваться для заполнения таблицы связи категорий и продуктов
-            $in_db_category[$category_name] = $category_id;
-            if ( $key = array_search( $category_name, $category_unique ) ) unset($category_unique[$key]);
-        }
+    // таблица категорий не имеет внешних ключей, добавление в БД начнём с неё
+    // сформируем запрос SELECT что бы знать какие разделы из файла XML уже записаны в БД
+    $query = "SELECT `category_id`,`category_name` 
+                FROM `a_category` 
+                WHERE `category_name` IN ("
+                . implode(',', array_fill( 1, count( $category_unique ), '?'  ) ) 
+                ." )";
+    foreach($category_unique as $key => $value)
+        $category_unique[$key] = &$category_unique[$key];
+    array_unshift( $category_unique, str_pad('', count($category_unique), 's') );
+    $stmt = $mysqli->prepare( $query );
+    call_user_func_array([$stmt, 'bind_param'], $category_unique);
+	$stmt->execute();
+    unset( $category_unique[0] );      
+    $stmt->bind_result ( $category_id, $category_name );
+    while ( $stmt->fetch() )
+    {
+        // сохраняем результат выборки, что бы не доставать эти записи повторно
+        // этот массив будет использоваться для заполнения таблицы связи категорий и продуктов
+        $in_db_category[$category_name] = $category_id;
+        if ( $key = array_search( $category_name, $category_unique ) ) unset($category_unique[$key]);
+    }
 
-        if ( !empty( $category_unique ) )
-        {
+    if ( !empty( $category_unique ) )
+    {
         // если остались не занесённые в БД разделы, то сформируем запрос INSERT IGNORE
         // на тот случай если кто-то другой произвёл вставку чуть раньше (поле `category_name` должно быть уникальным)
         $query = "INSERT IGNORE INTO `a_category` 
@@ -159,61 +162,66 @@ function importXml( string $a)
             // добавляем "свежие" категории в массив
             $in_db_category[$category_name] = $category_id;
         }
-        }
+    }
 
-        // добавляем товары в базу данных 
-        // поля `product_name` и `product_code` не уникальны
-        $query = "INSERT INTO `a_product` 
-                        ( `product_code`, `product_name` ) 
-                        VALUES ". implode(',', array_fill( 1, count( $product_bind )/2, '(?,?)'  ) );
-        foreach($product_bind as $key => $value)
-            $product_bind[$key] = &$product_bind[$key];
-        $stmt = $mysqli->prepare( $query );
-        call_user_func_array([$stmt, 'bind_param'], $product_bind);
-		$stmt->execute();
-        $first_id = $stmt->insert_id; // получаем идентификатор первой записи
-        // добавляем товару его идентификатор
-        foreach ( $product as $value )
+    // добавляем товары в базу данных 
+    // поля `product_name` и `product_code` не уникальны
+    $query = "INSERT INTO `a_product` 
+                    ( `product_code`, `product_name` ) 
+                    VALUES ". implode(',', array_fill( 1, count( $product_bind )/2, '(?,?)'  ) );
+    foreach( $product_bind as $key => $value )
+        $product_bind[$key] = &$product_bind[$key];
+    $stmt = $mysqli->prepare( $query );
+    call_user_func_array( [$stmt, 'bind_param'], $product_bind );
+	$stmt->execute();
+    $first_id = $stmt->insert_id; // получаем идентификатор первой записи
+    // добавляем товару его идентификатор
+    foreach ( $product as $value )
+    {
+        $prod_id[$first_id++] = $value;
+    }
+    $product = array_combine( array_keys( $product ), array_keys( $prod_id ) );
+    // формируем массив параметров для запроса к таблице связи товар - раздел
+    foreach ( $category as $value )
+    {
+        foreach ( $value as $key=>$val)
         {
-            $prod_id[$first_id++] = $value;
+            $prod_cat_bind[0] .= str_pad('', 2, 'ii');
+            $prod_cat_bind[] = (int)$product[$key];
+            $prod_cat_bind[] = (int)$in_db_category[$val];
         }
-        $product = array_combine( array_keys( $product ), array_keys( $prod_id ) );
+    }
+       
+    // добавляем информацию о принадлежности продукта к категории в таблицу связи
+    $query = "INSERT IGNORE INTO `a_prod_cat`
+                         ( `product_id`, `category_id` ) 
+                         VALUES ". implode(',', array_fill( 1, (count( $prod_cat_bind )-1)/2, '(?,?)'  ) );
+    foreach( $prod_cat_bind as $key => $value )
+        $prod_cat_bind[$key] = &$prod_cat_bind[$key];
+    $stmt = $mysqli->prepare( $query );
+    call_user_func_array( [$stmt, 'bind_param'], $prod_cat_bind );
+	$stmt->execute();
 
-        foreach ( $category as $value )
-        {
-            foreach ( $value as $key=>$val)
-            {
-                $prod_cat_bind[0] .= str_pad('', 2, 'ii');
-                $prod_cat_bind[] = $product[$key];
-                $prod_cat_bind[] = $in_db_category[$val];
-            }
-        }
-    echo '<pre><h3>prod_cat_bind</h3>';
-    print_r($prod_cat_bind);
-    echo '</pre>';       
+    // добавляем информацию о ценах на товары
+    $query = "INSERT INTO `a_price` 
+                     ( `product_id`, `price_type`, `price` ) 
+                     VALUES ". implode(',', array_fill( 1, (count( $price_bind )-1)/3, '(?,?,?)'  ) );
+    foreach( $price_bind as $key => $value )
+        $price_bind[$key] = &$price_bind[$key];
+    $stmt = $mysqli->prepare( $query );
+    call_user_func_array( [$stmt, 'bind_param'], $price_bind );
+	$stmt->execute();
         
-        // добавляем информацию о принадлежности продукта к категории в таблицу связи
- echo       $a_prod_cat = "INSERT IGNORE INTO `a_prod_cat`
-                             ( `product_id`, `categoryt_id` ) 
-                             VALUES ". implode(',', array_fill( 1, (count( $prod_cat_bind )-1)/2, '(?,?)'  ) );
-        
+    // добавляем информацию о свойствах товара
+    $query = "INSERT INTO `a_property` 
+                     ( `product_id`, `property_name`, `property_value` ) 
+                     VALUES ". implode(',', array_fill( 1, (count( $property_bind )-1)/3, '(?,?,?)'  ) );
+    foreach( $property_bind as $key => $value )
+        $property_bind[$key] = &$property_bind[$key];
+    $stmt = $mysqli->prepare( $query );
+    call_user_func_array( [$stmt, 'bind_param'], $property_bind );
+	$stmt->execute();
 
-
-
-
-        
-    echo '<pre><h3>category</h3>';
-    print_r($category);
-    echo '</pre>';
-    $a_product = "INSERT INTO `a_product` ( `product_id`, `product_code`, `product_name`, `category_id` ) VALUES ";
-    $a_property = "INSERT INTO `a_property` ( `property_id`, `product_id`, `property_name`, `property_value` ) VALUES ";
-    $a_price = "INSERT INTO `a_price` ( `price_id`, `product_id`, `price_name`, `price` ) VALUES ";
-
-    
-    
-    
-
-    
 }
 
 $import = importXml( 'Product.xml' );
