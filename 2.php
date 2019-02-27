2be561ddbe4532edd828253f728c90646ebdd040
KamenevVV <kam-val@mail.ru>
2019-02-27 11:41:55 +0300

    other commit

--- 2.php
+++ 2.php
@@ -376,14 +376,7 @@ function importXml( string $a)
         $report .='<p>- добавлено: '. $stmt->affected_rows .' связ'. num_end($stmt->affected_rows, ['ь','и','ей']) .'</p>';
         $stmt->close();
     }
-<<<<<<< HEAD
-    
-=======
-<<<<<<< HEAD
-=======
 
->>>>>>> 7cbd07767fafafd12a6afb4ee8d1a5d6d199bd7f
->>>>>>> step2
     // добавляем информацию о свойствах товара
     if ( !empty( $property_bind ) )
     {
